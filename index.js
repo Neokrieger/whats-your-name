4582a8356fd52bb4eaf03c9a89be799b422b9645
RyanVanDijck <rvandijck02@outlook.com>
2021-07-27 16:58:52 +0100

    Second requirement

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let player1Health = 50;
+let player2Health = 50;
+
+
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 
@@ -14,6 +18,8 @@ app.post('/name', (req, res) => {
   console.log("in the post method!")
   res.render('full-name.ejs', {
     player1Name: req.body.player1Name,
+    player1Health: player1Health,
+    player2Health: player2Health, 
     player2Name: req.body.player2Name
   })
 })
