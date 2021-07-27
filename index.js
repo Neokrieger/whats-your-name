da3a73b4f1e43131acf91a7df6fdb10f57bdfe6b
RyanVanDijck <rvandijck02@outlook.com>
2021-07-27 17:21:12 +0100

    Failing to add third requirement

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const port = 3000
 let player1Health = 50;
 let player2Health = 50;
 
+let player1Name = ""
+let player2Name = ""
+
+let counter = 0
+
+function decreasePlayerHealth(amount = 1){
+  console.log("Hit")
+  player1Health =- amount; 
+  counter++; 
+  app.post('/attack' + counter, (req,res) => {
+    res.render('full-name.ejs', {
+      player1Name: player1Name,
+      player1Health: player1Health,
+      player2Health: player2Health, 
+      player2Name: player2Name
+    })
+  })
+}
+
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
@@ -16,11 +35,14 @@ app.get('/', (req, res) => {
 
 app.post('/name', (req, res) => {
   console.log("in the post method!")
+  player1Name = req.body.player1Name
+  player2Name = req.body.player2Name
   res.render('full-name.ejs', {
     player1Name: req.body.player1Name,
     player1Health: player1Health,
     player2Health: player2Health, 
-    player2Name: req.body.player2Name
+    player2Name: req.body.player2Name,
+    decreasePlayerHealth: decreasePlayerHealth
   })
 })
 
