b61bd5b906d0c4408ce0e1e98303a69f3bf2b4b3
RyanVanDijck <rvandijck02@outlook.com>
2021-07-28 16:41:10 +0100

    Completing all required features and paralysis

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+const e = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
 
+let enemyPlayer; 
+
 class Player{
+  static isParalysed = false
   constructor(name, hitpoint = 50){
     this.name = name;
     this.hitpoint = hitpoint;
   }
 
-  damage(){
-    this.hitpoint -= 10;
+  damage(attack){
+    if(Player.isParalysed){
+      console.log("Paralysed");
+      Player.isParalysed = false; 
+      return 0; 
+    }
+    let damage; 
+    let chance = Math.random(); 
+    if (attack === "heavyAttack"){
+      console.log("Heavy Attack");
+      damage =  Math.round(Math.random() * 10);
+      Player.isParalysed = chance >= 0.8; 
+    }
+    else if(attack === "mediumAttack"){
+      console.log("Medium Attack");
+      damage = Math.round((Math.random() * 3) + 3);
+      Player.isParalysed = chance >= 0.9;
+    }
+    else{
+      console.log("Light Attack"); 
+      damage = 4; 
+      Player.isParalysed = chance >= 0.95;
+    }
+    this.hitpoint -= damage;
+    return damage;
   }
 
   isDead(){
@@ -44,39 +71,42 @@ app.post('/name', (req, res) => {
 })
 
 app.post('/attack1', (req, res) => {
-
-  player1.damage();
+  
+  console.log("Attack!")
+  console.log(`Player 1 loses ${player1.damage(req.body.Attack)} HP!`)
   if(player1.isDead()){
+    console.log("Player 1 Loses")
     res.send('<h1>Player 1 Loses</h1>')
   }
-  console.log("Player 1 loses 10 HP!")
-  console.log("Attack!")
-
-
-  res.render('attack1.ejs', {
-    player1Name: player1.name,
-    player2Name: player2.name,
-    player1Health: player1.hitpoint,
-    player2Health: player2.hitpoint,
-  })
+  
+    else{
+      res.render('attack1.ejs', {
+        player1Name: player1.name,
+        player2Name: player2.name,
+        player1Health: player1.hitpoint,
+        player2Health: player2.hitpoint,
+      })
+    }
 })
 
 app.post('/attack2', (req, res) => {
-
-  player2.damage();
+  console.log("Attack!")
+  console.log(`Player 2 loses ${player2.damage(req.body.Attack)} HP!`)
   if(player2.isDead()){
+    console.log("Player 2 Loses")
     res.send('<h1>Player 2 Loses</h1>')
   }
-  console.log("Player 2 loses 10 HP!")
-  console.log("Attack!")
+  
 
-
-  res.render('attack2.ejs', {
-    player1Name: player1.name,
-    player2Name: player2.name,
-    player1Health: player1.hitpoint,
-    player2Health: player2.hitpoint,
-  })
+  else{
+  
+    res.render('attack2.ejs', {
+      player1Name: player1.name,
+      player2Name: player2.name,
+      player1Health: player1.hitpoint,
+      player2Health: player2.hitpoint,
+    })
+  }
 })
 
 
