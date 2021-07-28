7c70c5122480654a124391b76455fbaf6c6206e7
Neokrieger <theneokrieger@hotmail.com>
2021-07-28 15:28:44 +0100

    Base requirements done

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let player1Health = 50;
-let player2Health = 50;
+class Player{
+  constructor(name, hitpoint = 50){
+    this.name = name;
+    this.hitpoint = hitpoint;
+  }
 
-let player1Name = ""
-let player2Name = ""
+  damage(){
+    this.hitpoint -= 10;
+  }
 
-let counter = 0
+  isDead(){
+    if(this.hitpoint <= 0) return true;
+  }
 
-function decreasePlayerHealth(amount = 1){
-  console.log("Hit")
-  player1Health =- amount; 
-  counter++; 
-  app.post('/attack' + counter, (req,res) => {
-    res.render('full-name.ejs', {
-      player1Name: player1Name,
-      player1Health: player1Health,
-      player2Health: player2Health, 
-      player2Name: player2Name
-    })
-  })
 }
 
+let player1;
+let player2;
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
@@ -34,17 +30,56 @@ app.get('/', (req, res) => {
 })
 
 app.post('/name', (req, res) => {
+
+  player1 = new Player(req.body.player1Name);
+  player2 = new Player(req.body.player2Name);
+
   console.log("in the post method!")
-  player1Name = req.body.player1Name
-  player2Name = req.body.player2Name
-  res.render('full-name.ejs', {
-    player1Name: req.body.player1Name,
-    player1Health: player1Health,
-    player2Health: player2Health, 
-    player2Name: req.body.player2Name,
-    decreasePlayerHealth: decreasePlayerHealth
+  res.render('attack1.ejs', {
+    player1Name: player1.name,
+    player2Name: player2.name,
+    player1Health: player1.hitpoint,
+    player2Health: player2.hitpoint,
   })
 })
+
+app.post('/attack1', (req, res) => {
+
+  player1.damage();
+  if(player1.isDead()){
+    res.send('<h1>Player 1 Loses</h1>')
+  }
+  console.log("Player 1 loses 10 HP!")
+  console.log("Attack!")
+
+
+  res.render('attack1.ejs', {
+    player1Name: player1.name,
+    player2Name: player2.name,
+    player1Health: player1.hitpoint,
+    player2Health: player2.hitpoint,
+  })
+})
+
+app.post('/attack2', (req, res) => {
+
+  player2.damage();
+  if(player2.isDead()){
+    res.send('<h1>Player 2 Loses</h1>')
+  }
+  console.log("Player 2 loses 10 HP!")
+  console.log("Attack!")
+
+
+  res.render('attack2.ejs', {
+    player1Name: player1.name,
+    player2Name: player2.name,
+    player1Health: player1.hitpoint,
+    player2Health: player2.hitpoint,
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
